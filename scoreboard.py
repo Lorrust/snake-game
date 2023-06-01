b3d3cae4a29c855fc8b8062a79b7876006e68756
Lucas <97752019+Lorrust@users.noreply.github.com>
2023-06-01 12:04:10 -0300

    Update scoreboard.py
    The scoreboard now registers the player's high score.

--- scoreboard.py
+++ scoreboard.py
@@ -2,11 +2,16 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 20, "normal")
 
+with open(r"Udemy\100 Days of Python\Snake Game\data.txt", mode="r") as data:
+    data_score = int(data.read())
+
 class Scoreboard(Turtle):
     
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open(r"Udemy\100 Days of Python\Snake Game\data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0,270)
@@ -14,13 +19,17 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
         
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
         
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open(r"Udemy\100 Days of Python\Snake Game\data.txt", mode="w") as data:
+                data.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
         
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
